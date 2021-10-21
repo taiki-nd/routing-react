d54daf5ba53ab2180d2befb8c8861a2a33f2331b
Taiki Noda <84260062+taiki-nd@users.noreply.github.com>
2021-10-21 21:49:36 +0900

    クエリパラメーター

--- src/UrlParameter.jsx
+++ src/UrlParameter.jsx
@@ -1,11 +1,15 @@
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 
 export const UrlParameter = () => {
   const { id } = useParams();
+  const { search } = useLocation();
+  const query = new URLSearchParams(search);
+  console.log(query);
   return (
     <div>
       <h1>Url Parameter</h1>
       <p>パラメパラメーターは{id}</p>
+      <p>クエリパラメーターは{query.get("name")}</p>
     </div>
   );
 };
--- src/page2.jsx
+++ src/page2.jsx
@@ -5,6 +5,8 @@ export const Page2 = () => {
     <div>
       <h1>Page2</h1>
       <Link to="/page2/999">URL Para</Link>
+      <br />
+      <Link to="/page2/999?name=hogehoge">Query Para</Link>
     </div>
   );
 };
