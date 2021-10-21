67e1e444eba38076fd8585ff68770c4a81deee71
Taiki Noda <84260062+taiki-nd@users.noreply.github.com>
2021-10-21 19:56:49 +0900

    Linkの設定

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { BrowserRouter, Link } from "react-router-dom";
+
 import { Home } from "./Home";
 import { Page1 } from "./Page1";
 import { Page2 } from "./page2";
@@ -5,10 +7,18 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <Home />
-      <Page1 />
-      <Page2 />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="page1">Page1</Link>
+        <br />
+        <Link to="page2">Page2</Link>
+
+        <Home />
+        <Page1 />
+        <Page2 />
+      </div>
+    </BrowserRouter>
   );
 }
