f311df1fde108b42c2b65791553c60ffed0879c5
Taiki Noda <84260062+taiki-nd@users.noreply.github.com>
2021-10-21 20:26:28 +0900

    ネスト

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 export const Page1 = () => {
   return (
     <div>
       <h1>Page1</h1>
+      <Link to="/page1/page1detailA">DetailA</Link>
+      <br />
+      <Link to="/page1/page1detailB">DetailB</Link>
     </div>
   );
 };
