3b6cd95d484567e9331e42d96c24700d200d389d
Taiki Noda <84260062+taiki-nd@users.noreply.github.com>
2021-10-21 19:51:48 +0900

    準備

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { Home } from "./Home";
+import { Page1 } from "./Page1";
+import { Page2 } from "./page2";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Home />
+      <Page1 />
+      <Page2 />
     </div>
   );
 }
